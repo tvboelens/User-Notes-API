ded0cae4daede64472a1ac933b194a6846fb2325
tvboelens <tvboelens@gmail.com>
2025-11-11 09:04:56 +0100

    Login service with test.

---
+++ controllers/auth_controllers.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"errors"
+	"net/http"
+
+	"user-notes-api/auth"
+	"user-notes-api/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController struct {
+	LoginService        services.LoginServiceIfc
+	RegistrationService services.RegistrationServiceIfc
+}
+
+func NewAuthController(login_service services.LoginServiceIfc, registration_service services.RegistrationServiceIfc) *AuthController {
+	controller := AuthController{LoginService: login_service, RegistrationService: registration_service}
+	return &controller
+}
+
+/* func (a *AuthController) Register(c *gin.Context) {
+	request_ctx := c.Request.Context()
+	token_string, err := a.RegistrationService.Register(request_ctx, credentials)
+} */
+
+func (a *AuthController) Login(c *gin.Context) {
+	var credentials auth.Credentials
+	err := c.Bind(&credentials)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+
+	request_ctx := c.Request.Context()
+	token_string, err := a.LoginService.Login(request_ctx, credentials)
+
+	wrongPwdError := new(services.ErrorWrongPassword)
+	wrongPwdError.Username = credentials.Username
+	if err != nil {
+		if errors.As(err, &wrongPwdError) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "wrong password"})
+			return
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"token": token_string})
+}
---
+++ controllers/auth_controllers_test.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"user-notes-api/auth"
+	"user-notes-api/testing/testutils/servicemocks"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+/* func TestAuthControllerRegistrationSuccess(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	mockService := new(testutils.MockAuthService)
+	mockService.On("Register", mock.Anything, mock.credentials).
+		Return(tt.mockReturn.token, tt.mockReturn.err)
+
+	authController := controllers.NewAuthController
+
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+
+	// set request and body here
+
+	authController.Register(ctx)
+
+	// now here have to check that w has the right data
+
+	mockService.AssertExpectations(t)
+
+} */
+
+func TestAuthControllerLoginSuccess(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	body := []byte(`{"username": "Alice", "password": "pwd"}`)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request, _ = http.NewRequest("POST", "/login", bytes.NewBuffer(body))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	mockLoginService := new(servicemocks.MockLoginService)
+	mockLoginService.On("Login", c.Request.Context(), auth.Credentials{Username: "Alice", Password: "pwd"}).Return("jwt", nil)
+
+	mockRegistrationService := new(servicemocks.MockRegistrationService)
+
+	authController := NewAuthController(mockLoginService, mockRegistrationService)
+
+	authController.Login(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "jwt")
+	mockLoginService.AssertExpectations(t)
+
+}
+
+func TestAuthControllerLoginFailure(t *testing.T) {
+
+}
+
+func TestAuthControllerLoginUserNotFound(t *testing.T) {
+
+}
